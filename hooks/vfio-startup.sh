459195a10b10674b7442362a8b33bb7eb4246414
je <je@h>
2022-07-10 18:11:47 +0800

    init

---
+++ hooks/vfio-startup.sh
@@ -0,0 +1,27 @@
+set -x
+
+systemctl stop gdm
+
+echo 0 > /etc/class/vtconsole/vtcon0/bind
+echo 0 > /etc/class/vtconsole/vtcon1/bind
+
+echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
+sleep 3
+
+modprobe -r nvidia_drm
+modprobe -r nvidia_uvm
+modprobe -r nvidia_modeset
+modprobe -r drm_kms_helper
+modprobe -r nvidia
+modprobe -r i2c_nvidia_gpu
+modprobe -r drm
+
+virsh nodedev-detach pci_0000_00_01_0
+virsh nodedev-detach pci_0000_01_00_0
+virsh nodedev-detach pci_0000_01_00_1
+
+modprobe vfio_pci
+modprobe vfio
+modprobe vfio_iommu_type1
+modprobe vfio_virqfd
