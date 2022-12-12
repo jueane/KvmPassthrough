70a2f244588d67b669e6ea0c73b74c02344e3d47
je <je@h>
2022-12-12 12:23:59 +0800

    ..

--- hooks/vfio-startup.sh
+++ hooks/vfio-startup.sh
@@ -12,14 +12,27 @@ echo 0 > /sys/class/vtconsole/vtcon0/bind
 echo 0 > /sys/class/vtconsole/vtcon1/bind
 
 # Unbind EFI-Framebuffer
-echo vesa-framebuffer.0 > /sys/bus/platform/drivers/vesa-framebuffer/unbind
+# echo vesa-framebuffer.0 > /sys/bus/platform/drivers/vesa-framebuffer/unbind
+echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
 # Avoid a Race condition by waiting 2 seconds. This can be calibrated to be shorter or longer if required for your system
-sleep 2
+sleep 5
+
+
+modprobe -r nvidia_drm
+modprobe -r nvidia_uvm
+modprobe -r nvidia_modeset
+modprobe -r drm_kms_helper
+modprobe -r nvidia
+modprobe -r i2c_nvidia_gpu
+modprobe -r drm
 
 # Unbind the GPU from display driver
 virsh nodedev-detach pci_0000_01_00_0
 virsh nodedev-detach pci_0000_01_00_1
 
 # Load VFIO Kernel Module  
-modprobe vfio-pci  
+modprobe vfio_pci
+modprobe vfio
+modprobe vfio_iommu_type1
+modprobe vfio_virqfd
--- hooks/vfio-teardown.sh
+++ hooks/vfio-teardown.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 set -x
+
+
+sleep 10
+
+
+modprobe -r vfio_pci
+modprobe -r vfio
+modprobe -r vfio_iommu_type1
+modprobe -r vfio_virqfd
   
 # Re-Bind GPU to Nvidia Driver
 virsh nodedev-reattach pci_0000_01_00_0
 virsh nodedev-reattach pci_0000_01_00_1
 
-# Reload nvidia modules
-# modprobe nvidia
-# modprobe nvidia_modeset
-# modprobe nvidia_uvm
-# modprobe nvidia_drm
-modprobe nouveau
 
 # Rebind VT consoles
-echo 1 > /sys/class/vtconsole/vtcon0/bind
+echo 1 > /etc/class/vtconsole/vtcon0/bind
+echo 1 > /etc/class/vtconsole/vtcon1/bind
+
 # Some machines might have more than 1 virtual console. Add a line for each corresponding VTConsole
 #echo 1 > /sys/class/vtconsole/vtcon1/bind
 
-nvidia-xconfig --query-gpu-info > /dev/null 2>&1
-echo vesa-framebuffer.0 > /sys/bus/platform/drivers/vesa-framebuffer/bind
 
+# echo vesa-framebuffer.0 > /sys/bus/platform/drivers/vesa-framebuffer/bind
+echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/bind
+
+
+# modprobe nouveau
+modprobe nvidia
+modprobe nvidia_modeset
+modprobe nvidia_uvm
+modprobe nvidia_drm
+modprobe drm_kms_helper
+modprobe i2c_nvidia_gpu
+modprobe drm
+
+startx
 # Restart Display Manager
 # systemctl start display-manager.service
